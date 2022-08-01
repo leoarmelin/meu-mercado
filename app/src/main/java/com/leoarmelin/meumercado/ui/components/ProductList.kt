79b9d541a43c194e9abef5d49b6da8a5c21c3844
leoarmelin <leonardo99.armelin@gmail.com>
2022-07-31 21:01:28 -0300

    feat(product-list): add bottom items

--- app/src/main/java/com/leoarmelin/meumercado/ui/components/ProductList.kt
+++ app/src/main/java/com/leoarmelin/meumercado/ui/components/ProductList.kt
@@ -1,10 +1,7 @@
 package com.leoarmelin.meumercado.ui.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
@@ -12,11 +9,19 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.leoarmelin.meumercado.extensions.getLocalDateTime
 import com.leoarmelin.meumercado.models.Ticket
 import com.leoarmelin.meumercado.ui.theme.Gray400
+import com.leoarmelin.meumercado.ui.theme.Metropolis
 import com.leoarmelin.meumercado.ui.theme.Secondary50
+import java.time.format.DateTimeFormatter
 
 @Composable
 fun ProductList(ticket: Ticket) {
@@ -44,8 +49,51 @@ fun ProductList(ticket: Ticket) {
                 modifier = Modifier.padding(top = 4.dp)
             )
         }
+
         items(ticket.items) { product ->
             ProductItem(product)
+        }
+
+        item {
+            Text(
+                text = buildAnnotatedString {
+                    withStyle(SpanStyle(fontWeight = FontWeight.Bold)) {
+                        append("Quantidade total: ")
+                    }
+                    append(if (ticket.quantity == 1) "1 item" else "${ticket.quantity} itens")
+                },
+                fontFamily = Metropolis,
+                fontSize = 16.sp,
+                color = Gray400,
+                modifier = Modifier
+                    .padding(top = 8.dp, start = 24.dp, end = 24.dp)
+                    .fillMaxWidth()
+            )
+
+            Text(
+                text = buildAnnotatedString {
+                    withStyle(SpanStyle(fontWeight = FontWeight.Bold)) {
+                        append("Método: ")
+                    }
+                    append(ticket.payment_method)
+                },
+                fontFamily = Metropolis,
+                fontSize = 16.sp,
+                color = Gray400,
+                modifier = Modifier
+                    .padding(top = 8.dp, start = 24.dp, end = 24.dp)
+                    .fillMaxWidth()
+            )
+
+            Text(
+                style = MaterialTheme.typography.h5,
+                text = ticket.date.getLocalDateTime()
+                    .format(DateTimeFormatter.ofPattern("dd/MM/yyyy")),
+                color = Gray400,
+                modifier = Modifier
+                    .padding(top = 8.dp, start = 24.dp, end = 24.dp)
+                    .fillMaxWidth()
+            )
         }
     }
 }
